b71294950017d744a863d171139fc85ebcf2d657
TornikeTt <chargeishvilitornike@gmail.com>
2021-04-22 18:18:49 +0400

    change class to function

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React from 'react';
 import './App.css';
 import Box from "./Box.js"
 
-class App extends React.Component {
-  render() { 
-    return (
-      <div className="App"> 
-            <div className="container">  <Box /> </div>
-      </div>
-    )
-  }
-
+function App() {
+  return (
+    <div className="App"> 
+        <div className="container">  <Box /> </div>
+    </div>
+  )
 }
 
 export default App	
